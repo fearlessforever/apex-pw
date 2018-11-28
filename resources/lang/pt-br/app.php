37fb2c178af4f0e1ee02addfe680e48a0040493e
Pedro Lima <pedro-octavio@hotmail.com>
2018-11-28 21:09:14 -0200

    Added pt_BR lang

---
+++ resources/lang/pt-br/app.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'my_profile' => 'Minha Conta',
+    'profile' => 'Dados cadastrais',
+    'authentication' => 'Autenticação',
+    'change_password' => 'Alterar senha',
+    'password' => 'Senha',
+    'current_password' => 'Senha atual',
+    'new_password' => 'Nova Senha',
+    'update' => 'Atualizar',
+    'confirm' => 'Confirmar',
+    'menu' => [
+        'home' => 'Ínicio',
+        'donate' => 'Doações',
+        'support' => 'Suporte',
+        'profile' => 'Minha Conta',
+    ],
+];
